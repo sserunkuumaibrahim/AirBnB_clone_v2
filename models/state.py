6e91d1c3797897189775074afd71f31091d1e1f1
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-06-25 00:49:13 +0100

    Bug free!

--- models/state.py
+++ models/state.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    if getenv("HBNB_TYPE_STORAGE") = 'db':
+        __table__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade="all, delete", backref="states")
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if getenv("HBNB_TYPE_STORAGE") != 'db':
+        @property
+        def cities(self):
+            """ Returns the saved cities """
+            city_values = models.storage.all("City").values()
+            city_list = []
+            for city in city_values:
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -5,10 +5,10 @@ from models.state import State
 
 
 class test_state(test_basemodel):
-    """ """
+    """ tests for state module """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ Tests the init class of State """
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
@@ -16,4 +16,6 @@ class test_state(test_basemodel):
     def test_name3(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertEqual(type(new.name), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
